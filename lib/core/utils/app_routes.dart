82475e0144efb10f9d74198ebbcbb1ec2df72689
mkha2 <mkh515574@gmail.com>
2025-08-14 16:42:34 +0300

    + add app_colors + add app_routes + add app_text_style + add app_theme for theming and navigation

---
+++ lib/core/utils/app_routes.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import '../../features/home/view/home_screen.dart';
+
+class AppRoutes{
+  static const String homeRouteName = "HomeScreen";
+
+  static Map<String,WidgetBuilder> routes = {
+    homeRouteName: (context) => const HomeScreen(),
+  };
+}
---
+++ lib/core/utils/app_text_style.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:news_app/core/utils/app_colors.dart';
+
+class AppTextStyle {
+  static final TextStyle medium24Black = GoogleFonts.inter(
+    fontSize: 24,
+    fontWeight: FontWeight.w500,
+    color: AppColors.blackColor,
+  );
+  static final TextStyle medium24White = GoogleFonts.inter(
+    fontSize: 24,
+    fontWeight: FontWeight.w500,
+    color: AppColors.blackColor,
+  );
+  static final TextStyle medium20White = GoogleFonts.inter(
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    color: AppColors.blackColor,
+  );
+  static final TextStyle medium20Black = GoogleFonts.inter(
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    color: AppColors.blackColor,
+  );
+}
